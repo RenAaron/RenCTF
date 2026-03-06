7ad00ee87d6651fa05104cc47200b2c73b555979
renaaron <renaaronellis@gmail.com>
2026-03-05 20:03:26 -0500

    fixing JWT issue

--- Matrix/scripts/main/_test.py
+++ Matrix/scripts/main/_test.py
@@ -4,3 +4,7 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate('ren-ctf-webapp-firebase-adminsdk-cqnpv-648f74930c.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+doc_ref = db.collection("games").document("TIC-TAC-TOE")
+state_ref = db.collection("matrix_state").document("STATE")
+users_ref = db.collection("users")
