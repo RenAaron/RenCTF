4aa90dddb1e4d031f4d79fd3f5ed0a71b02f813e
renaaron <renaaronellis@gmail.com>
2026-05-11 14:46:54 -0400

    leaderboard user-cards continued! very cool yes yes!

--- Webapp/ren-ctf-webapp/src/components/leaderboard-components/UserCard.js
+++ Webapp/ren-ctf-webapp/src/components/leaderboard-components/UserCard.js
@@ -3,52 +3,82 @@ import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const colors = {
-    Red: '#ff2200',
-    Blue: '#00aeff',
+    Red: '#ff0080',
+    Blue: '#006eff',
     Hard: '#ff0022',
     Medium: '#ffa600',
     Easy: '#77ff00',
+    Purple: '#ce79ff'
 }
 
 const borderStyles = {
-    Red: '1px dashed #ff2200',
-    Blue: '1px dashed #0066ff',
+    Red: '2px solid #ff006f',
+    Blue: '2px solid #0066ff',
 }
 
 const borderFill = {
-    Red: '#ff22001f',
+    Red: '#ff00951f',
     Blue: '#0059ff12'
 }
 
 const User = ({ user }) => {
+
+    function getRandomInt(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+    }
     const userTeam = user.team;
 
     return (
-        <div style={{border: borderStyles[userTeam], margin: '10px', padding: '5px', backgroundColor: borderFill[userTeam], borderRadius: '10px'}}>
+        <div style={{border: borderStyles[userTeam], margin: '10px', padding: '5px', backgroundColor: borderFill[userTeam], borderRadius: '5x'}}>
             <div style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
-                <img style ={{width: '90px', marginRight: '10px'}} src=""></img>
+                <img className="pixel-art" style ={{width: '120px', marginRight: '10px'}} src={`https://raw.githubusercontent.com/RenAaron/RenCTF/refs/heads/main/Webapp/ren-ctf-webapp/public/icons/pfps/face_${getRandomInt(1,10)}/${userTeam}.gif`}></img>
                 <h1>{user.display_name}</h1>
 
                 <h2 style={{color: colors[userTeam]}}>&nbsp;({userTeam})</h2>
             </div>
 
-            <hr/>
+            <hr style={{color: colors[userTeam], margin: '5px'}}/>
 
-        <h2>
-            Completed:  
-        </h2>
+            <h2>
+                Season Score:  <t style={{color: colors['Purple']}}>{user.total_moves}</t>
+            </h2>
 
-        {user.comp_chal["easy"].length > 0 && <h3>
-            <t style={{color: colors['Easy']}}>Easy: </t> {user.comp_chal["easy"]}
-        </h3>}
+            <hr style={{color: colors[userTeam], margin: '5px'}}/>
 
-        {user.comp_chal["medium"].length > 0 && <h3>
-            <t style={{color: colors['Medium']}}>Medium: </t> {user.comp_chal["medium"]}
-        </h3>}
+            <h2>
+                Completed:  
+            </h2>
 
-        {user.comp_chal["hard"].length > 0 && <h3>
-            <t style={{color: colors['Hard']}}>Hard: </t> {user.comp_chal["hard"]}
-        </h3>}
+            {user.comp_chal["easy"].length > 0 && <h3>
+                <t style={{color: colors['Easy']}}>Easy: </t> {user.comp_chal["easy"]}
+            </h3>}
+
+            {user.comp_chal["medium"].length > 0 && <h3>
+                <t style={{color: colors['Medium']}}>Medium: </t> {user.comp_chal["medium"]}
+            </h3>}
+
+            {user.comp_chal["hard"].length > 0 && <h3>
+                <t style={{color: colors['Hard']}}>Hard: </t> {user.comp_chal["hard"]}
+            </h3>}
+
+            <h2 style={{display: 'flex', alignItems: 'center', margin: '5px'}}>
+                Badges: 
+                <img src={`/icons/${userTeam}.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Fire.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Discord.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Diamond.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Github.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/CRed.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Matrix.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/cube_2.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Home.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Emerald.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+                <img src={`/icons/Skull.gif`} alt="Pixel Art" className="pixel-art" width={30} height={30}/>
+            </h2>
+
+            
 
             {/* <t className="user-stat" style={{ color: colors[user.team[0]] }}>
                 {"("}
